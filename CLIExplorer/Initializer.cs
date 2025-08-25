4da45a48b433e376cffba0d525afa4794c7091c6
Badi <wayw04321@gmail.com>
2025-08-25 05:31:43 +0300

    1.4.1: added try-catch in Initializer.cs for looking same prefixs.

--- CLIExplorer/Initializer.cs
+++ CLIExplorer/Initializer.cs
@@ -1,4 +1,5 @@
 ﻿using CLIExplorer.Commands;
+using CLIExplorer.Utils;
 using System;
 using System.Linq;
 
@@ -8,33 +9,29 @@ namespace CLIExplorer
     {
         public static void InitCommands()
         {
-            // add commands prefix with how to create them in lambda.
-
-            CommandHandler.avaibleCommands.Add(LsCommand.CommandPrefix, () => new LsCommand());
-            CommandHandler.avaibleCommands.Add(CwdCommand.CommandPrefix, () => new CwdCommand());
-            CommandHandler.avaibleCommands.Add(ExitCommand.CommandPrefix, () => new ExitCommand());
-            CommandHandler.avaibleCommands.Add(EchoCommand.CommandPrefix, () => new EchoCommand());
-            CommandHandler.avaibleCommands.Add(CdCommand.CommandPrefix, () => new CdCommand());
-            CommandHandler.avaibleCommands.Add(ClearCommand.CommandPrefix, () => new ClearCommand());
-            CommandHandler.avaibleCommands.Add(MvCommand.CommandPrefix, () => new MvCommand());
-            CommandHandler.avaibleCommands.Add(MkdirCommand.CommandPrefix, () => new MkdirCommand());
-            CommandHandler.avaibleCommands.Add(RmdirCommand.CommandPrefix, () => new RmdirCommand());
-            CommandHandler.avaibleCommands.Add(CpCommand.CommandPrefix, () => new CpCommand());
-
-            // add commands prefix with how to create them in lambda.
-
-            var avaiblePrefixs = CommandHandler.avaibleCommands.Keys;
-
-            foreach (var prefix in avaiblePrefixs)
+            try
             {
-                if (avaiblePrefixs.Count(p => p == prefix) > 1) // looks for if there more occurances of that prefix more than 1.
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine($"WARNING: There is multiple prefix occurances for {prefix}. " +
+                // add commands prefix with how to create them in lambda.
+
+                CommandHandler.avaibleCommands.Add(LsCommand.CommandPrefix, () => new LsCommand());
+                CommandHandler.avaibleCommands.Add(CwdCommand.CommandPrefix, () => new CwdCommand());
+                CommandHandler.avaibleCommands.Add(ExitCommand.CommandPrefix, () => new ExitCommand());
+                CommandHandler.avaibleCommands.Add(EchoCommand.CommandPrefix, () => new EchoCommand());
+                CommandHandler.avaibleCommands.Add(CdCommand.CommandPrefix, () => new CdCommand());
+                CommandHandler.avaibleCommands.Add(ClearCommand.CommandPrefix, () => new ClearCommand());
+                CommandHandler.avaibleCommands.Add(MvCommand.CommandPrefix, () => new MvCommand());
+                CommandHandler.avaibleCommands.Add(MkdirCommand.CommandPrefix, () => new MkdirCommand());
+                CommandHandler.avaibleCommands.Add(RmdirCommand.CommandPrefix, () => new RmdirCommand());
+                CommandHandler.avaibleCommands.Add(CpCommand.CommandPrefix, () => new CpCommand());
+                CommandHandler.avaibleCommands.Add(CpCommand.CommandPrefix, () => new CpCommand());
+
+                // add commands prefix with how to create them in lambda.
+            }
+            catch (ArgumentException)
+            {
+                ColorWrite.WriteColored(ConsoleColor.Yellow, $"WARNING: There is some multiple prefix occurances. " +
                         $"Most likely, only the command with the same prefix that was initialized first will run. " +
                         $"If you have modified the code, please take this warning into consideration.");
-                    Console.ResetColor();
-                }
             }
         } 
     }
